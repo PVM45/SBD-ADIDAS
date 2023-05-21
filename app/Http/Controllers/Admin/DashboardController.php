da2870c5bf51d0fcb5bb3c6d4d798e3178e599f4
SymphonyArt <117836194+SymphonyArt@users.noreply.github.com>
2023-05-21 20:28:51 +0700

    ya

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,16 +5,60 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\produk;
-
+use App\Models\User;
+use TCPDF;
+use Illuminate\Support\Facades\Redirect;
 class DashboardController extends Controller
 {
     public function index() {
         $jumlahProduk = produk::count();
-        return view('layouts.admin.dashboard',compact('jumlahProduk'));
+        $users = User::where('id', '!=', 1)->get();
+        $jumlahUser = $users->count();
+        return view('layouts.admin.dashboard',compact('jumlahProduk','jumlahUser'));
     }
 
     public function run() {
         return view ('layouts.admin.addkategori');
           }
-    
+          public function generate()
+          {
+              // Ambil data yang dibutuhkan untuk laporan (misalnya dari database atau variabel lainnya)
+              $jumlahProduk = produk::count();
+              $users = User::where('id', '!=', 1)->get();
+              $jumlahUser = $users->count();
+              $jumlahPesanan = 200;
+              $omset = 215000;
+      
+              // Buat objek TCPDF
+              $pdf = new TCPDF();
+      
+              // Set dokumen informasi dan header
+              $pdf->SetCreator('ADIDAS');
+              $pdf->SetAuthor('Kelompok 2');
+              $pdf->SetTitle('Laporan Dashboard Adidas');
+              $pdf->SetHeaderData('', 0, 'Laporan Dashboard Adidas', '');
+      
+              // Buat halaman
+              $pdf->AddPage();
+              $logoPath = storage_path('app/public/bg.jpeg');
+              $logoWidth = 80;  // Lebar logo yang diinginkan
+              $logoHeight = 60; // Tinggi logo yang diinginkan
+              $logoX = ($pdf->getPageWidth() - $logoWidth) / 2;
+              $logoY = ($pdf->getPageHeight() - $logoHeight) / 2;
+              $pdf->Image($logoPath, $logoX, $logoY, $logoWidth, $logoHeight, 'jpeg', '', '', true, 300, '', false, false, 0);
+              // Tambahkan konten ke halaman
+              $html = '
+                  <h1>Laporan</h1>
+                  <p>Jumlah Produk: ' . $jumlahProduk . '</p>
+                  <p>Jumlah User: ' . $jumlahUser . '</p>
+                  <p>Jumlah Pesanan: ' . $jumlahPesanan . '</p>
+                  <p>Omset: Rp' . number_format($omset, 2) . '</p>
+              ';
+              $pdf->writeHTML($html, true, false, true, false, '');
+      
+              $pdf->Output(storage_path('app/public/laporan.pdf'), 'F');
+
+        // Redirect kembali ke halaman sebelumnya
+        return Redirect::back();
+          }
 }
--- resources/views/layouts/admin/dashboard.blade.php
+++ resources/views/layouts/admin/dashboard.blade.php
@@ -45,7 +45,7 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                        <a href="{{route('admin.generate.report')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
 
@@ -98,7 +98,7 @@
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"></div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $jumlahUser }}</div>
                                                 </div>
                                                
                                             </div>
