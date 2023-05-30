562425807094e71643503824ffea9b100e249c27
Fawner45 <fahmihussaini45@gmail.com>
2023-05-30 21:42:56 +0700

    p

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -17,56 +17,56 @@ class productController extends Controller
 
     public function index()
     {
-        //cek
+//cek
         $kategoris = Kategori::all();
-        $subkategoris = Subkategori::all();
-        $produks = produk::all();
+    $subkategoris = Subkategori::all();
+      $produks = produk::all();
         return view('layouts.author.produk', compact('produks', 'kategoris', 'subkategoris'));
-        $kategoris = Kategori::all();
-        $subkategoris = Subkategori::all();
+$kategoris = Kategori::all();
+    $subkategoris = Subkategori::all();
         $produks = Produk::all();
         return view('frontend.frontend_layout.product_page.products', compact('produks', 'kategoris', 'subkategoris'));
     }
 
     public function show($id)
-    {
+{
         $kategoris = Kategori::all();
-        $subkategoris = Subkategori::all();
-        $produk =  kategoris_subkategoris::where('produk_id', $id)->get();
-        $produks = Komentar::where('produk_id', $id)->get();
-        $produksr = Rating::where('produk_id', $id)->take(1)->get();
-        $limit =  Produk::where('id', '!=', $id)->latest()->take(3)->get();
-        return view('frontend.frontend_layout.product_page.single_product', compact('produk', 'produks', 'limit', 'produksr', 'kategoris', 'subkategoris'));
+    $subkategoris = Subkategori::all();
+    $produk =  kategoris_subkategoris::where('produk_id', $id)->get();
+    $produks = Komentar::where('produk_id', $id)->get();
+    $produksr = Rating::where('produk_id', $id)->take(1)->get();
+    $limit =  Produk::where('id','!=',$id)->latest()->take(3)->get();
+    return view('frontend.frontend_layout.product_page.single_product', compact('produk','produks','limit','produksr', 'kategoris', 'subkategoris'));
+
+}
+
+public function search(Request $request)
+{  $kategoris = Kategori::all();
+    $subkategoris = Subkategori::all();
+    $keyword = $request->input('keyword');
+    $products = Produk::where('nama_produk',  'LIKE', "%$keyword%")->get();
+    if ($products->isEmpty()) {
+        return view('produk.not-found');
     }
+    return view('produk.search', compact('products', 'kategoris', 'subkategoris'));
+}
 
-    public function search(Request $request)
-    {
-        $kategoris = Kategori::all();
-        $subkategoris = Subkategori::all();
-        $keyword = $request->input('keyword');
-        $products = Produk::where('nama_produk',  'LIKE', "%$keyword%")->get();
-        if ($products->isEmpty()) {
-            return view('produk.not-found');
-        }
-        return view('produk.search', compact('products', 'kategoris', 'subkategoris'));
-    }
+public function filter(Request $request)
+{
+    $kategoris = Kategori::all();
+    $subkategoris = Subkategori::all();
+    $kategoriId = $request->input('kategori');
+    $subkategoriId = $request->input('subkategori');
 
-    public function filter(Request $request)
-    {
-        $kategoris = Kategori::all();
-        $subkategoris = Subkategori::all();
-        $kategoriId = $request->input('kategori');
-        $subkategoriId = $request->input('subkategori');
+    // Dapatkan objek kategori dan subkategori berdasarkan ID
+    $kategori = Kategori::findOrFail($kategoriId);
+    $subkategori = Subkategori::findOrFail($subkategoriId);
 
-        // Dapatkan objek kategori dan subkategori berdasarkan ID
-        $kategori = Kategori::findOrFail($kategoriId);
-        $subkategori = Subkategori::findOrFail($subkategoriId);
+    // Lakukan filter berdasarkan kategori dan subkategori yang dipilih
+    $produk = Produk::whereHas('subkategoris', function ($query) use ($subkategoriId) {
+        $query->where('subkategori_id', $subkategoriId);
+    })->where('kategori_id', $kategoriId)->get();
 
-        // Lakukan filter berdasarkan kategori dan subkategori yang dipilih
-        $produk = Produk::whereHas('subkategoris', function ($query) use ($subkategoriId) {
-            $query->where('subkategori_id', $subkategoriId);
-        })->where('kategori_id', $kategoriId)->get();
-
-        return view('produk.filterproduk', compact('produk', 'kategoris', 'subkategoris'));
-    }
+    return view('produk.filterproduk', compact('produk', 'kategoris', 'subkategoris'));
+}
 }
